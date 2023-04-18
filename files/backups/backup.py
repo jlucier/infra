488e4c1c17b6ff742d9285b7674e0ac64e7d5439
Jordan Lucier <jordan@jordanlucier.com>
2023-04-17 21:36:41 -0400

    Automated local backups. Need to add remote

---
+++ files/backups/backup.py
@@ -0,0 +1,267 @@
+#! /usr/bin/env python3.11
+
+import argparse
+import logging
+import os
+import secrets
+import shlex
+import subprocess
+import tomllib
+from datetime import datetime, timedelta
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+
+"""
+Install:
+    - download borg 2.0 release binary, move to /usr/local/lib
+    - python3.11 (apt)
+"""
+
+LOG_FMT = "[%(name)s][%(asctime)s][%(levelname)s]: %(message)s"
+logging.basicConfig(format=LOG_FMT)
+logger = logging.getLogger("backups")
+logger.setLevel(logging.DEBUG)
+
+AUTO_PREFIX = "auto"
+BACKUP_PROPERTY = "backup:done"
+BORG_SUCCESS = 0
+BORG_WARN = 1
+BORG_ERROR = 2
+BORG_PASSPHRASE = None
+BORG_KEYFILE_EXPORT_DIR = None
+
+
+def failexit(reason: str):
+    logger.error(reason)
+    raise RuntimeError("failed")
+
+
+def execute(cmd: str):
+    return subprocess.check_output(
+        shlex.split(cmd), text=True, stderr=subprocess.STDOUT
+    )
+
+
+def borg_cmd(cmd: str, env: dict | None = None, check: bool = True, **kwargs):
+    p = subprocess.run(
+        shlex.split(cmd), env=env, text=True, capture_output=True, **kwargs
+    )
+
+    if check:
+        output = []
+        if p.stdout:
+            output.append(p.stdout)
+        if p.stderr:
+            output.append(p.stderr)
+
+        output = "\n".join(output)
+
+        match p.returncode:
+            case 2:
+                failexit(f"Borg command failed fatally\n{output}")
+            case 1:
+                logger.warning(f"Borg command warning\n{output}")
+
+    return p
+
+
+def list_snapshots(fs: str):
+    return execute(f"zfs list -H -t snapshot -o name {fs}").strip().split("\n")
+
+
+def is_snapshot_backed_up(snapshot: str):
+    try:
+        return bool(int(execute(f"zfs get {BACKUP_PROPERTY} -o value -H {snapshot}")))
+    except ValueError:
+        return False
+
+
+def mark_snapshot_backed_up(snapshot: str):
+    execute(f"zfs set {BACKUP_PROPERTY}=1 {snapshot}")
+
+
+def zfs_destroy(item: str):
+    assert "@" in item, f"Oh boy, destroy {item}?"
+    execute(f"zfs destroy {item}")
+
+
+def get_fs_mountpoint(fs: str):
+    return execute(f"zfs list -o mountpoint -H {fs}").strip()
+
+
+def parse_archive_tag(tag: str):
+    dash = tag.index("-")
+    # prefix (type), time
+    return tag[:dash], datetime.fromisoformat(tag[dash + 1 :])
+
+
+def find_last_snapshot(fs: str, prefix: str):
+    snaps = filter(
+        lambda t: t[0].startswith(prefix), map(parse_archive_tag, list_snapshots(fs))
+    )
+    snaps = sorted(snaps, key=lambda t: t[1], reverse=True)
+    return snaps[0] if len(snaps) else None
+
+
+def snapshot(fs: str, name: str):
+    full_name = f"{fs}@{name}"
+    execute(f"zfs snapshot {full_name}")
+    return name
+
+
+def get_repo_from_fs(repo_prefix: str, fs: str):
+    return str(Path(repo_prefix).expanduser() / fs.replace("/", "_"))
+
+
+def make_borg_env(repo: str):
+    return {**os.environ, "BORG_PASSPHRASE": BORG_PASSPHRASE, "BORG_REPO": repo}
+
+
+def do_backup(repo_prefix: str, snapshot: str, conf: dict):
+    fs, snap_name = snapshot.split("@")
+    _, dt = parse_archive_tag(snap_name)
+
+    repo = get_repo_from_fs(repo_prefix, fs)
+    snap_dir = Path(get_fs_mountpoint(fs)) / ".zfs/snapshot/" / snap_name
+
+    borg_env = make_borg_env(repo)
+
+    # check if repo exists, create if not
+    rinfo = borg_cmd("borg rinfo", env=borg_env, check=False)
+    if rinfo.returncode == 2:
+        # repo dne, create
+        logger.info(f"Creating new repo for {fs}")
+        borg_cmd("borg rcreate --encryption repokey-aes-ocb", env=borg_env)
+    elif rinfo.returncode != 0:
+        failexit(f"Unexpected return from borg rinfo {rinfo.returncode}")
+
+    logger.info(f"Backing up snaphot {snapshot}")
+    opts = [
+        "--exclude-caches",
+        "--compression=zstd",
+        f"--timestamp={dt.isoformat()}",
+    ]
+
+    if conf.get("exclude"):
+        opts.extend(f"--exclude={e}" for e in conf["exclude"])
+
+    opts = " ".join(opts)
+    res = borg_cmd(f"borg create {opts} '{{now}}' .", cwd=snap_dir, env=borg_env)
+    logger.info(f"Output:\n{res.stdout}\n{res.stderr}")
+
+
+# Main
+
+
+def initial_setup(config: dict):
+    global BORG_PASSPHRASE, BORG_KEYFILE_EXPORT_DIR
+
+    # create or read passphrase
+    pass_file = Path(config["borg_pass_file"]).expanduser()
+    if not pass_file.is_file():
+        logger.warning("Passphrase file not preset, creating new passphrase")
+        BORG_PASSPHRASE = secrets.token_hex(16)
+        pass_file.write_text(BORG_PASSPHRASE)
+        pass_file.chmod(0o600)
+
+    BORG_PASSPHRASE = pass_file.read_text()
+
+    # create key export dir
+    BORG_KEYFILE_EXPORT_DIR = Path(config["keyfile_export_dir"]).expanduser()
+    BORG_KEYFILE_EXPORT_DIR.mkdir(exist_ok=True)
+
+    # create repo dir
+    Path(config["repo_prefix"]).expanduser().mkdir(exist_ok=True)
+
+
+def perform_backups(config: dict, fs_conf: dict):
+    fs = fs_conf["fs"]
+    logger.info(f"Working on {fs}...")
+
+    dt = datetime.now()
+    snap = f"{AUTO_PREFIX}-{dt.isoformat()}"
+    logger.info(f"Creating snapshot {fs}@{snap}")
+    snapshot(fs, snap)
+
+    # only consider auto snapshots we create
+    snaps = [
+        s for s in list_snapshots(fs) if s[s.index("@") + 1 :].startswith(AUTO_PREFIX)
+    ]
+
+    for snap in snaps:
+        if not is_snapshot_backed_up(snap):
+            do_backup(config["repo_prefix"], snap, fs_conf)
+            mark_snapshot_backed_up(snap)
+
+    logger.info(f"Cleaning snapshots for {fs}")
+
+    for snap in snaps:
+        _, snap_dt = parse_archive_tag(snap[snap.index("@") + 1 :])
+        if dt - snap_dt > timedelta(days=config["keep_snapshot_days"]):
+            logger.info(f"Dropping old snapshot {snap}")
+            zfs_destroy(snap)
+
+    borg_env = make_borg_env(get_repo_from_fs(config["repo_prefix"], fs))
+
+    logger.info(f"Borg prune for {fs}")
+    day = config["day"]
+    week = config["week"]
+    month = config["month"]
+    borg_cmd(
+        f"borg prune --keep-daily={day} --keep-weekly={week} --keep-monthly={month}",
+        env=borg_env,
+    )
+
+    logger.info(f"Borg compact {fs}")
+    borg_cmd("borg compact", env=borg_env)
+
+    logger.info(f"Finished {fs}")
+
+
+def main(args):
+    if not int(execute("id -u")):
+        failexit("Don't as root!")
+
+    if args.config.get("log_file"):
+        fh = RotatingFileHandler(
+            Path(args.config["log_file"]).expanduser(), maxBytes=int(1e6), backupCount=2
+        )
+        fh.setFormatter(logging.Formatter(LOG_FMT))
+        logger.addHandler(fh)
+
+    borg_version = execute("borg --version").strip()
+    logger.info(f"Borg version: {borg_version}")
+    logger.info("Starting up...")
+
+    initial_setup(args.config)
+
+    for fs_conf in args.config["fs"].values():
+        perform_backups(args.config, fs_conf)
+
+    logger.info("Complete!")
+
+
+def clean_snaps(args):
+    for fs_conf in args.config["fs"].values():
+        for snap in list_snapshots(fs_conf["fs"]):
+            if input(f"Delete {snap}? (y/N) ").lower() == "y":
+                zfs_destroy(snap)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Backups")
+    parser.add_argument(
+        "-l",
+        "--log-file",
+        type=lambda x: Path(x).expanduser(),
+        default=Path.home() / "backups",
+        help="Path to use for log file (rotated)",
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=lambda x: tomllib.load(Path(x).expanduser().open("rb")),
+        default=tomllib.load((Path.home() / "backups/config.toml").open("rb")),
+        help="Path to config file",
+    )
+    main(parser.parse_args())
